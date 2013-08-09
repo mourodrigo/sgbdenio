cb669d48c89d24080d07477f3869474c60d95673
Andrey <andreybramos@hotmail.com>
2013-08-09 13:16:25 -0300

    Retirado include não usado.

--- src/sgbdenio.cpp
+++ src/sgbdenio.cpp
@@ -15,7 +15,6 @@
 #include <vector>
 #include <sstream>
 #include <utility>
-//#include <sys/dir.h>
 #include <dirent.h>
 #include <limits.h>
 #include <unistd.h>
@@ -42,7 +41,7 @@ private:
     string name;
     string dir;
     bool defaultDb;
-    
+
 public:
     int getId(){
         return this->id;
@@ -68,8 +67,8 @@ public:
     void setDefault(bool newDefault){
         this->defaultDb = newDefault;
     }
-    
-    
+
+
     Database(int newId, string newName, string newDir, bool newDefaultDb){
         this->id = newId;
         this->name = newName;
@@ -86,7 +85,7 @@ private:
     int size;
     string value;
     bool optional;
-    
+
 public:
     string getName(){
         return this->name;
@@ -108,21 +107,21 @@ public:
     void setSize(int newSize){
         this->size = newSize;
     }
-    
+
     string getValue(){
         return this->value;
     }
     void setValue(string newValue){
         this->value = newValue;
     }
-    
+
     bool getOptional(){
         return this->optional;
     }
     void setOptional(bool newOptional){
         this->optional = newOptional;
     }
-    
+
     Attribute(string newName, int newType, int newSize, string newValue, bool newOptional){
         this->name = newName;
         this->type = newType;
@@ -139,7 +138,7 @@ vector<string> explode( const string& s, const string& delimiter ){ //explode
     vector<string> result;
     string::size_type from = 0;
     string::size_type to = 0;
-    
+
     while ( to != string::npos ){
         to = s.find( delimiter, from );
         if ( from < s.size() && from != to ){
@@ -184,7 +183,7 @@ bool fileExists(const char *filename){ //verifica se arquivo existe
     }else{
         return false;
     }
-    
+
 }
 
 #pragma mark -sgbd
@@ -215,15 +214,15 @@ vector<Database> getAllDatabase(){
                     dbs.push_back(dbReaded);
 		        }
 		    }
-            
+
             file.close();
-            
+
             return dbs;
 		}else{
             cout << "Erro ao abrir arquivo do banco de dados";
             return dbs;
 		}
-        
+
 	}else{
 		cout << "arquivo nao existe";
 	}
@@ -231,15 +230,15 @@ vector<Database> getAllDatabase(){
 }
 
 bool createDatabase(Database newdb){
-    
+
 	int dbid = 0;
     dbid = newdb.getId();
-    
+
 	if(newdb.getName().length() == 0){
 		return false;
 	}
-    
-    
+
+
 	if(fileExists(dbpath)){
 		vector <Database> dbs;
 	    dbs = getAllDatabase();
@@ -250,38 +249,38 @@ bool createDatabase(Database newdb){
             		dbs.at(x).setDefault(0);
             	}
             }
-            
+
         }
-        
-        
+
+
     	if(newdb.getDir().length() == 0){
     		newdb.setDir(getCurrentPath()+"/"+newdb.getName());
     	}else{
             newdb.setDir(newdb.getDir()+"/"+newdb.getName());
         }
-        
+
         newdb.setId(indexIdNewDatabase);
         dbs.push_back(newdb);
-        
+
         ofstream file;
-        
+
         file.open(dbpath);
-        
+
         for (int x = 0; x<dbs.size(); x++){
     		file << dbs.at(x).getId() << separator << dbs.at(x).getName() << separator << dbs.at(x).getDir()  << separator << dbs.at(x).getDefault() << "\n";
         }
-        
+
         file.close();
-        
+
         string strPath = newdb.getDir();
-        
+
         cout << "\n\n\n a criar " << strPath << endl;
-        
+
         MKDIR(strPath.c_str());
-        
+
         if(log){cout << "Bando de dados " << newdb.getName() << " com id 0 no diretorio: \n " << newdb.getDir() << "\n  default 1 " << endl;} //inserção no arquivo esta ok
-        
-        
+
+
         return true;
 	}else{
 		ofstream file;
@@ -294,10 +293,10 @@ bool createDatabase(Database newdb){
         MKDIR(newdb.getDir().c_str());
 		file << 0 << separator << newdb.getName() << separator << newdb.getDir()  << separator << 1 << "\n";
 		file.close();
-        
+
         if(log){cout << "Bando de dados " << newdb.getName() << " com id 0 no diretorio: \n " << newdb.getDir() << "\n  default 1 " << endl;} //inserção no arquivo esta ok
-        
-        
+
+
 		return true;
 	}
 }
@@ -321,46 +320,46 @@ Database getDefaultDb(){
 
 
 int main() {
-    
-    
-    
-    /*  //CRIANDO ALGUNS DATABASES
-     
+
+
+
+      //CRIANDO ALGUNS DATABASES
+
      Database dbmouro2(6666,"mouro1","",false);
      createDatabase(dbmouro2);
-     
-     
+
+
      Database dbmouro(6666,"sgbdeniooo","",true);
      createDatabase(dbmouro);
-     
-     
+
+
      Database dbmouro3(6666,"mouro2","/Users/Desenvolvimento/Desktop",true);
      createDatabase(dbmouro3);
-     
+
      Database dbmouro4(6666,"moueo3","/Users/Desenvolvimento/Desktop",false);
      createDatabase(dbmouro4);
-     
-     */
-    
-    
-    
-    
+
+
+
+
+
+
     //SETANDO DATABASE DEFAULT
-    
+
     Database defaultDb = getDefaultDb(); //declarando defaultdb aqui pois nao consegui declarar com global ainda
-    
+
     cout << "db default: \n" << defaultDb.getId() << " - " << defaultDb.getName() << " - " << defaultDb.getDir();
-    
-    
-    
+
+
+
 	//ABAIXO COMENTARIOS PARA NAO ESQUECER A SINTAXE DAS COISAS
-    
-    
+
+
     //	vector<Database> dbs; //vetor de classes do tipo database
-    
-    
+
+
     //	cout << "Hello" << endl; // prints Hello
-    
+
     /*
      cout << "ints size: " << ints.size() << endl;
      ints.push_back(123);
@@ -368,47 +367,47 @@ int main() {
      ints.pop_back();
      cout << "ints size: " << ints.size() << endl;
      */
-    
+
     /*
      Database db1(1,"banco1","/var/www/",true);  //instancia db1 do tipo database e seta os valores através do método construtor --> Database(int newId, string newName, string newDir, bool newDefaultDb){
      Database db2(2,"banco222","/var/www/db1/",false);
      Database db3(3,"banco3333","/users/mouro/banco3/",false);
      Database db4(4,"banco44444","/root/db4",false);
-     
+
      dbs.push_back(db1); //manda db1 pro vetor
-     
+
      for (int x = 0; x<dbs.size(); x++){
      cout << "id: " << dbs.at(x).getId() << "name: "<< dbs.at(x).getName()  << " dir: "<< dbs.at(x).getDir() << " default: " << dbs.at(x).getDefault() << endl;
      }
-     
+
      createDatabase(db1); //CHAMA FUNCAO CREATEDATABASE QUE POR ENQUANTO SÓ VERIFICA SE UM ARQUIVO EXISTE
      createDatabase(db2);
      createDatabase(db3);
-     
-     
+
+
      /*   //TENTATIVA DE GRAVAR O VETOR DE BANCOS NO ARQUIVO, AINDA NAO FUNCIONA
-     
+
      ostream outpuf_file("dbs.data");
      std:ofstream_iterator <Database> output_iterator(outpuf_file, "\n");
      //	cout << "db " << dbs.at(2) << endl;
      */
-    
+
     /*
      std::vector<std::string> example;
      example.push_back("this");
      example.push_back("is");
      example.push_back("a");
      example.push_back("test");
-     
+
      std::ofstream output_file("./example.txt");
      std::ostreambuf_iterator<std::string> output_iterator_tag;
      std::copy(example.begin(), example.end(), output_iterator_tag);
-     
+
      */
-    
+
     //	cout << "database name: " << db1.getName() << "dir: " << db1.getDir() << endl;
-    
+
     system("pause");
 	return 0;
-    
+
 }
